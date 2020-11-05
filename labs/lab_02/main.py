a414913be093dfcccd5255008da918e34f322276
bakaevam <43135896+bakaevam@users.noreply.github.com>
2020-11-05 20:39:19 +0300

    our noise (#11)
    * gauss_ver1
    
    * noise
    
    * release version

--- labs/lab_02/main.py
+++ labs/lab_02/main.py
@@ -4,6 +4,17 @@ from time import perf_counter
 from utils import mse, reshape_img
 from median_filter import median_filter
 from gauss_filter import gauss_filter
+from noise import noise
+
+
+def test_noise(img):
+    print('\t[Loading...] Calculating..')
+    start = perf_counter()
+    result = noise(img, -9, 20)
+    finish = perf_counter()
+    cv2.imshow('Add noise', result)
+    print('\tTime: ' + str(finish - start))
+    return result
 
 
 def test_median_filter(img):
@@ -13,6 +24,8 @@ def test_median_filter(img):
     finish = perf_counter()
     cv2.imshow('Median Filter', result)
     print('\tTime: ' + str(finish - start))
+    mse_value = mse(img, result)
+    print('\tMSE: ' + str(mse_value) + '\n')
 
 
 def test_gauss_filter(img):
@@ -22,6 +35,8 @@ def test_gauss_filter(img):
     finish = perf_counter()
     cv2.imshow('Gauss Filter', result)
     print('\tTime: ' + str(finish - start))
+    mse_value = mse(img, result)
+    print('\tMSE: ' + str(mse_value) + '\n')
 
 
 def test_opencv_bilateral_filter(img):
@@ -32,10 +47,12 @@ def test_opencv_bilateral_filter(img):
     finish = perf_counter()
     cv2.imshow('OpenCV Bilateral Filter', result)
     print('\tTime: ' + str(finish - start))
+    mse_value = mse(img, result)
+    print('\tMSE: ' + str(mse_value) + '\n')
 
 
 def main():
-    path_to_img = r'..\..\resources\noise.jpg'
+    path_to_img = r'..\..\resources\flower.jpg'
 
     print('========== NOICE SUPPRESSION ==========\n')
     print('Src image: ' + path_to_img + '\n')
@@ -44,16 +61,18 @@ def main():
     cv2.imshow('Src image', img)
 
     print('I. Making noise\n')
+    noise_img = test_noise(img)
+    cv2.waitKey()
 
     print('II. Deleting noise')
     print('\tII.a Median filter')
-    test_median_filter(img)
+    test_median_filter(noise_img)
     cv2.waitKey()
     print('\tII.b Gauss filter')
-    test_gauss_filter(img)
+    test_gauss_filter(noise_img)
     cv2.waitKey()
     print('\tII.c OpenCV Bilateral filter')
-    test_opencv_bilateral_filter(img)
+    test_opencv_bilateral_filter(noise_img)
     cv2.waitKey()
 
     cv2.destroyAllWindows()
